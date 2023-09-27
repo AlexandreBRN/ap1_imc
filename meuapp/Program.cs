0dcaa106fe46a74a290a43aaed30cf1b8457e9ec
AlexandreBR <130804109+AlexandreBRN@users.noreply.github.com>
2023-09-26 22:18:19 -0300

    Update Program.cs

--- meuapp/Program.cs
+++ meuapp/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 class Program
 {
@@ -6,30 +6,33 @@ class Program
     {
         while (true)
         {
+        // Solicita o nome, idade, altura e peso da pessoa ao usuário e armazena em uma variável.
             Console.Write("Digite o nome da pessoa: ");
             string nome = Console.ReadLine();
-
             Console.Write("Digite a idade: ");
             int idade = int.Parse(Console.ReadLine());
-
             Console.Write("Digite a altura (em metros): ");
             double altura = double.Parse(Console.ReadLine());
-
             Console.Write("Digite o peso (em kg): ");
             double peso = double.Parse(Console.ReadLine());
 
+            // Calcula o IMC da pessoa usando a função Calcular IMC.
             double imc = CalcularIMC(peso, altura);
+            // Determina a categoria do IMC e a faixa etaria.
             string categoriaIMC = DeterminarCategoriaIMC(imc);
             string faixaEtaria = DeterminarFaixaEtaria(idade);
 
+            // Exibe o relatório com os dados coletados.
             Console.WriteLine("\nRelatório:");
             Console.WriteLine($"Nome: {nome}");
             Console.WriteLine($"Idade: {idade} anos");
             Console.WriteLine($"IMC: {imc:F2} ({categoriaIMC})");
             Console.WriteLine($"Faixa Etária: {faixaEtaria}");
 
+            // Pergunta ao usuário se deseja gerar um novo relatório para outra pessoa.
             Console.Write("\nDeseja gerar um novo relatório para outra pessoa? (S/N): ");
             string resposta = Console.ReadLine();
+            // Se a resposta não for "S" (ou seja, não for "Sim"), sai do loop.
             if (resposta.ToUpper() != "S")
             {
                 break;
@@ -37,11 +40,13 @@ class Program
         }
     }
 
+    // Função para calcular o IMC com base no peso e altura fornecidos.
     static double CalcularIMC(double peso, double altura)
     {
         return peso / (altura * altura);
     }
 
+    // Função para determinar a categoria do IMC com base no valor do IMC.
     static string DeterminarCategoriaIMC(double imc)
     {
         if (imc < 18.5)
@@ -82,5 +87,7 @@ class Program
         }
     }
 }
+
+
 
 
